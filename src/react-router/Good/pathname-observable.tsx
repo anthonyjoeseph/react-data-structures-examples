062540542bc228499b7b60f4fae239347fa6afa8
Anthony Gabriele <anthonyjoeseph@gmail.com>
2022-03-15 11:55:23 -0400

    cleanup

--- src/react-router/Good/pathname-observable.tsx
+++ src/react-router/Good/pathname-observable.tsx
@@ -2,10 +2,10 @@ import { BehaviorSubject, fromEvent, merge } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export const mutablePathname = () => {
-  const reroutes = new BehaviorSubject(window.location.pathname)
   const onPopState = fromEvent(window, 'popstate').pipe(
     map(() => window.location.pathname)
   )
+  const reroutes = new BehaviorSubject(window.location.pathname)
   reroutes.subscribe((newRoute) => window.history.pushState(null, '', newRoute))
   return {
     pathname$: merge(onPopState, reroutes),
